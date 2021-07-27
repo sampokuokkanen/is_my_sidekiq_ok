e65d2aa7b1608108bf4ee3351888633a4c57220d
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-07-27 22:17:28 +0900

    Remove unneeded filess

--- app/helpers/is_my_sidekiq_ok/application_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module IsMySidekiqOk
-  module ApplicationHelper
-  end
-end
--- app/jobs/is_my_sidekiq_ok/application_job.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module IsMySidekiqOk
-  class ApplicationJob < ActiveJob::Base
-  end
-end
